3f1a012b1e8e83ecf855b9b17e2fdd5ac662dbe6
JomaEstrada <jose.estrada.madera@gmail.com>
2017-03-10 10:08:50 -0700

    Primer commit calc

---
+++ ejercicio04/src/pages/home/home.ts
@@ -0,0 +1,48 @@
+//Integrantes>
+//Jose Mar
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public resultado:string='';
+  public n1:any;
+  public n2:any;
+  public nu1:number=0;
+  public nu2:number=0;
+  public simbolo:string='';
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  suma(){
+    this.nu1=parseInt(this.n1);
+    this.nu2=parseInt(this.n2);
+    this.resultado = this.nu1+this.nu2+'';
+    this.simbolo="+";
+  }
+  resta(){
+    this.resultado= this.nu1- this.nu2+'';
+    this.simbolo="-";
+  }
+
+  division(){
+    //this.resultado= this.n1/ this.n2+'';
+    this.simbolo="/";
+  }
+  multiplicacion(){
+    //this.resultado= this.n1 * this.n2+'';
+    this.simbolo="*";
+  }
+
+  modulo(){
+    //this.resultado= this.n1% this.n2+'';
+    this.simbolo="%";
+  }
+
+
+}
